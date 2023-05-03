258335ce1585c7fcb8835b78b3de4fd80c064ecb
Barbara Moroni <116181111+Barbiinnha@users.noreply.github.com>
2023-05-03 10:15:00 -0300

    Add files via upload

---
+++ main (1).rb
@@ -0,0 +1,54 @@
+possibilidades = { 
+    'Arad' => ['Zerind', 'Sibiu', 'Timisoara'],
+    'Pitesti' => ['Rimnicu Vilcea', 'Craiova', 'Bucharest'],
+    'Urziceni' => ['Bucharest', 'Hirsova', 'Vaslui'],
+    'Eforie' => ['Hirsova'],
+    'Lugoj' => ['Timisoara', 'Mehadia'],
+    'Oradea' => ['Zerind', 'Sibiu'],
+    'Neamt' => ['Iasi'],
+    'Bucharest' => ['Fagaras', 'Pitesti', 'Giurgiu', 'Urziceni'],
+    'Giurgiu' => ['Bucharest'],
+    'Zerind' => ['Oradea', 'Arad'],
+    'Sibiu' => ['Oradea', 'Arad', 'Fagaras', 'Rimnicu Vilcea'],
+    'Mehadia' => ['Lugoj', 'Dobreta'],
+    'Iasi' => ['Vaslui', 'Neamt'],
+    'Fagaras' => ['Sibiu', 'Bucharest'],
+    'Rimnicu Vilcea' => ['Sibiu', 'Craiova', 'Pitesti'],
+    'Timisoara' => ['Arad', 'Lugoj'],
+    'Dobreta' => ['Mehadia', 'Craiova'],
+    'Hirsova' => ['Urziceni', 'Eforie'],
+    'Craiova' => ['Dobreta', 'Rimnicu Vilcea', 'Pitesti'],
+    'Vaslui' => ['Urziceni', 'Iasi']
+}
+
+#recebe como parametro a cidade origem, cidade destino e as possivilidades de conexao
+def buscar_caminho(origem, destino, possibilidades)
+    visitados = [origem] #lista origem
+    fila = [origem]#fila origem
+    parentes = {} #armazenar o nó pai
+
+    while fila.length > 0 #fila vazia
+        no = fila.shift  #remocao e movimentação de nos
+        if no == destino
+            caminho = [no] #lista que armazena o caminho
+            while caminho[0] != origem
+                caminho.unshift(parentes[caminho[0]])
+            end
+            return caminho # retorna caminho encontrado
+        end
+        possibilidades[no].select {|vizinho| !visitados.include?(vizinho)} #selecao vizinhos
+                          .each do |vizinho|
+            visitados.push(vizinho) #add vizinho
+            fila.push(vizinho)
+            parentes[vizinho] = no
+        end
+    end
+    return nil #n econtra o caminho retorna
+end
+
+origem = 'Arad' # --> a cidade de origem (ponto de partida)
+destino = 'Bucharest' # --> a cidade final (objetivo)
+
+caminho = buscar_caminho(origem, destino, possibilidades) #se encontra 
+puts "O caminho mais rápido entre #{origem} e #{destino} é >
+#{caminho.join(' --> ')}."
